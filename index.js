df11033dc87b87067794625cf101b0db8dead89b
sahalmohamed1 <sahal.mohamed1@student.moringaschool.com>
2026-04-03 12:25:41 +0300

    Initial commit

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
+require('datejs');
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users: [],
+    merge_date: "4/3/2026"
+  };
 
+  for (let arr of args) {
+    combinedObject.users.push(...arr);
+  }
+
+  combinedObject.merged_date = new Date().toString('MM/dd/yyyy');
+
+  return combinedObject;
+}
+
+const result = combineUsers(
+  [
+    { name: 'Alice', age: 30 },
+    { name: 'Bob', age: 25 }
+  ],
+  [
+    { name: 'Charlie', age: 35 },
+    { name: 'David', age: 40 }
+  ]
+);
+
+console.log(result);
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
